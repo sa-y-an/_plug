69f348b8f3883c6b69a93a230866ce59ad65c2e6
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-12 21:49:16 +0530

    basic frontend

--- src/components/CustomLayout.js
+++ src/components/CustomLayout.js
@@ -1,24 +1,33 @@
 import '../css/customlayout.css';
 import React from 'react'
-import { Layout, Menu } from 'antd';
+import { Layout, Menu,  Pagination } from 'antd';
 import Post from './Post';
-const { Header, Content, Footer } = Layout;
+const { Header, Content,} = Layout;
 
 function CustomLayout() {
   return (
     <div>
-    <Layout className="layout">
+    <Layout className="layout" >
         <Header>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{justifyContent:'flex-end'}}>
             {new Array(2).fill(null).map((_, index) => {
             const key = index + 1;
             return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
             })}
+            
+
         </Menu>
+        
         </Header>
-        <Content style={{ padding: '0 50px' }}>
+        <Content style={{ padding: '0 50px' , height: '100vh'}}
+        align="middle" >
+            <h2>
+              Welcome to the feed !
+            </h2>
             <Post/>
+            <br />
+            <Pagination defaultCurrent={1} total={50} />
         </Content>
         
     </Layout>,
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import '../css/post.css';
-import { Skeleton, Card,Avatar } from 'antd';
+import { Skeleton, Card,Avatar, Row, Col } from 'antd';
 import { UserOutlined, HeartOutlined, DislikeOutlined, LikeOutlined} from '@ant-design/icons';
 const { Meta } = Card;
+
 
 function Post({name, desription, message, photoUrl}) {
   return (
     <div className='post'>
-        <Card
-          style={{ width: 300, marginTop: 16 }}
-          actions={[
-            <LikeOutlined/> ,
-            <HeartOutlined />,
-            <DislikeOutlined />,
-            
-          ]}
-        >
-          <Skeleton loading={false} avatar active>
-            <Meta
-              avatar={<Avatar icon={<UserOutlined />} />}
-              title="Card title"
-              description="This is the description"
-        />
-          </Skeleton>
-        </Card>
+      <Row type="flex" align="middle" style={{justifyContent: 'center'}}>
+        <Col >
+          <Card
+            style={{ width: 300, marginTop: 16, horizontalAlign: 'middle',
+          }}
+            actions={[
+              <LikeOutlined/> ,
+              <HeartOutlined />,
+              <DislikeOutlined />,
+              
+            ]}
+          >
+            <Skeleton loading={false} avatar active>
+              <Meta
+                avatar={<Avatar icon={<UserOutlined />} />}
+                title="Card title"
+                description="This is the description"
+          />
+            </Skeleton>
+          </Card>
+        </Col>
+      </Row>
 
     </div>
   )
