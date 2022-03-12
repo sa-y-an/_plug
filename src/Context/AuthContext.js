f7a61c648da53b5a37fafc70cf42ca64547df7dd
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-13 04:35:34 +0530

    Login Context

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -16,6 +16,7 @@ export const AuthContextProvider = ( {children}) => {
     const [state, dispatch] = useReducer( authReducer, {
         user : null
     })
+    console.log('AuthContextState: ', state)
 
     return (
         <AuthContext.Provider value={{ ...state , dispatch}}>
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -2,16 +2,24 @@ import React from 'react'
 import { firebase } from '../Firebase/firebase'
 import { Button, Space, Layout, Menu } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
-const { Header, Content } = Layout;
+import { useAuthContext } from '../hooks/useAuthContext'
 
+const { Header, Content } = Layout;
 
 function Signin() {
     
+    const { dispatch } = useAuthContext()
+
     const SignInWithFireBase = () => {
         var google_provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(google_provider)
-        .then( (re) => {
-            console.log(re);
+        .then( (res) => {
+            // console.log(re);
+
+            // dispatch login event 
+            dispatch( { type: 'LOGIN' , payload : res.user })
+
+
         })
         .catch( (err) => {
             console.log(err);
