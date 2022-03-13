9bb31c53627c9e1db8a7a6c3a0190c10a7f37d7c
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-13 06:26:01 +0530

    refactor

--- src/components/CustomLayout.js
+++ src/components/CustomLayout.js
@@ -1,26 +1,11 @@
 import "../css/customlayout.css";
 import React from "react";
-import { Layout, Menu, Pagination } from "antd";
-import Post from "./Post";
-import Header2 from "./Header";
-
-
-
-const { Content } = Layout;
+import Feed from "./Feed";
 
 function CustomLayout() {
   return (
     <div>
-      <Layout className="layout">
-        <Header2/>
-        <Content style={{ padding: "0 50px", height: "100vh" }} align="middle">
-          <h2>Welcome to the feed !</h2>
-          <Post />
-          <br />
-          <Pagination defaultCurrent={1} total={50} />
-        </Content>
-      </Layout>
-      ,
+      <Feed/>
     </div>
   );
 }
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Layout, Pagination } from "antd";
+import Post from "./Post";
+import Header2 from "./Header";
+const { Content } = Layout;
 
 function Feed() {
   return (
     <div>
-        Hello
+      <Layout className="layout">
+        <Header2/>
+        <Content style={{ padding: "0 50px", height: "100vh" }} align="middle">
+          <h2>Welcome to the feed !</h2>
+          <Post />
+          <br />
+          <Pagination defaultCurrent={1} total={50} />
+        </Content>
+      </Layout>
     </div>
   )
 }
