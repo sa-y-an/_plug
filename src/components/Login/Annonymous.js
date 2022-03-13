8aa625f1f4b8d3395160daf1ed5ea49f33dc0f09
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-14 05:12:20 +0530

    annnonymous user

--- src/components/Login/Annonymous.js
+++ src/components/Login/Annonymous.js
@@ -2,20 +2,41 @@ import React from 'react'
 import { Content } from 'antd/lib/layout/layout'
 import { Button } from 'antd'
 import { UnlockOutlined } from '@ant-design/icons'
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 function Annonymous() {
-  return (
+
+    const { dispatch } = useAuthContext()
+
+    const loginUser = () => {
+        const auth = getAuth();
+        signInAnonymously(auth)
+        .then((res) => {
+            // Signed in..
+            dispatch( { type: 'LOGIN' , payload : res.user })
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ...
+        });    
+    }
+
+
+    return (
     <div>
         <Content>
             <Button
             icon={<UnlockOutlined />}
             type="primary"
+            onClick={loginUser}
             >
                 Annonymous Login
             </Button>
         </Content>
     </div>
-  )
+    )
 }
 
 export default Annonymous
