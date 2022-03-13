54b227d43cddfae843a8b2c7727e9925a7dcc2e5
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-13 15:34:44 +0530

    dummy

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,12 +1,29 @@
+import "../css/account.css"
 import React from 'react'
 import {Layout} from 'antd'
+import { Form, Input, Button, Select } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+
+
 function Account() {
   return (
     <div>
       <Layout align="middle">
-        <h5>
-          Account
-        </h5>
+        <h3>
+          Set Your Status 
+        </h3>
+        <Content>
+          <Form align="middle">
+            <Form.Item
+              name = ""
+            >
+              <Input style={{ maxWidth : "60vw"}}/>
+            </Form.Item>
+            <Button type='primary'> 
+                Submit
+              </Button>
+          </Form>
+        </Content>
       </Layout>
     </div>
   )
