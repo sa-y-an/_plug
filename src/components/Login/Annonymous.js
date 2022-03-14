6c03a05f886567d2b02eea19dd34c58934b3f375
sa-y-an <sayan.ee.nitd@gmail.com>
2022-03-14 05:41:49 +0530

    annonymous login

--- src/components/Login/Annonymous.js
+++ src/components/Login/Annonymous.js
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Content } from 'antd/lib/layout/layout'
 import { Button } from 'antd'
 import { UnlockOutlined } from '@ant-design/icons'
-import { getAuth, signInAnonymously } from "firebase/auth";
+import { getAuth, signInAnonymously , updateProfile} from "firebase/auth";
 import { useAuthContext } from '../../hooks/useAuthContext';
+import FetchRandomData from './FetchRandomData';
 
 function Annonymous() {
 
     const { dispatch } = useAuthContext()
+    let name = "Johny";
+    let picUrl = "https://randomuser.me/api/portraits/thumb/women/42.jpg"
+
+    useEffect( () => {
+        fetch(`https://randomuser.me/api/`)
+        .then( (res) => {
+            return res.json();
+        } )
+        .then( (data) => {
+            name = data.results[0].name.first;
+            picUrl = data.results[0].picture.thumbnail;
+        })
+    }, [])
 
     const loginUser = () => {
         const auth = getAuth();
         signInAnonymously(auth)
         .then((res) => {
             // Signed in..
+            updateProfile(auth.currentUser, {
+                displayName: name, 
+                photoURL: picUrl
+              }).then(() => {
+                // Profile updated!
+                // ...
+              }).catch((error) => {
+                // An error occurred
+                // ...
+              });
+              
             dispatch( { type: 'LOGIN' , payload : res.user })
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ...
         });    
     }
 
@@ -27,6 +51,7 @@ function Annonymous() {
     return (
     <div>
         <Content>
+            <FetchRandomData/>
             <Button
             icon={<UnlockOutlined />}
             type="primary"
